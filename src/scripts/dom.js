e3ea93f4c55a1729cab2b86bab7a1380a4fe2df8
Samuel <samuferfort@gmail.com>
2024-04-25 14:11:00 +0200

    improved layout and bug fixes

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,12 +1,19 @@
 export default function displayData(data) {
-    const text = document.querySelector(".weather-text");
-    const city = document.querySelector(".weather-city");
-    const temp = document.querySelector(".weather-temp");
-    const icon = document.querySelector(".weather-icon");
-    const date = document.querySelector(".weather-date");
-  
-    text.innerHTML = `${data.condition}`;
-    city.innerHTML = `${data.city}`;
-    temp.innerHTML = `${data.temp_c}°C`;
-    date.innerHTML = `${data.date}`;
-  }
+  const text = document.createElement("div");
+  const city = document.querySelector(".weather-city");
+  const tempC = document.querySelector(".c");
+  const tempF = document.querySelector(".f");
+  const img = document.createElement("img");
+  const date = document.querySelector(".weather-date");
+  const textDiv = document.querySelector(".weather-text");
+  textDiv.innerHTML = "";
+
+  text.innerHTML = `${data.condition}`;
+  city.innerHTML = `${data.city}, ${data.country}`;
+  tempC.innerHTML = `${data.temp_c}°C`;
+  tempF.innerHTML = `${data.temp_f}°F`;
+  date.innerHTML = `${data.date}`;
+  img.src = `${data.icon}`;
+  textDiv.appendChild(text);
+  textDiv.appendChild(img);
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,16 @@
 import "../styles.css";
 import getData from "./data.js";
 import "../assets/weather.jpg";
-import "../assets/search.png";
+import searchImg from "../assets/search.png";
 import displayData from "./dom.js";
 
 const search = document.querySelector(".search");
 const input = document.querySelector("input");
+const searchPng = document.querySelector(".searchImg");
 search.addEventListener("click", async () => {
+  if (!input.value) return;
   const weatherData = await getData(input.value);
   displayData(weatherData);
 });
 
-
+searchPng.src = searchImg;
